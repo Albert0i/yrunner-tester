6c50d78d723519ca97e7d37944206ef36f8f69e2
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2023-05-03 11:52:14 +0800

    config fix

---
+++ src/config/apiGateway.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const apiGateway = axios.create({
+    baseURL: 'http://localhost:8989/api/v2/yr',
+    timeout: 60* 1000, // in milliseconds
+    headers: {'Authorization': 'Bearer VEUdEii4n7nCvofaBRJEC'}
+  });
+
+
+/*
+   npm | axios
+   https://www.npmjs.com/package/axios
+*/
