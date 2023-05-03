a6adeec432c1ce5079886540085db091fe1caf12
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2023-05-03 11:31:34 +0800

    add more files

--- src/App.js
+++ src/App.js
@@ -9,3 +9,8 @@ function App() {
 }
 
 export default App;
+
+/*
+   Install Tailwind CSS with Create React App
+   https://tailwindcss.com/docs/guides/create-react-app
+*/
---
+++ src/config/dbConfig.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    user: process.env.NODE_ORACLEDB_USER,
+    password: process.env.NODE_ORACLEDB_PASSWORD,
+    connectString:process.env.NODE_ORACLEDB_CONNECTIONSTRING
+};
+
+/*
+   node-oracledb/examples
+   https://github.com/oracle/node-oracledb/tree/main/examples
+*/
