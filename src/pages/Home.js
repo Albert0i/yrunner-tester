a7812985bf53ccafc962a1b39d850205ac3580a6
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2023-05-09 11:32:15 +0800

    add sth

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
 import { apiGateway } from '../config/apiGateway'
 import { useStopwatch } from "react-use-precision-timer"
+import { useLocalStorage } from '../hook/useLocalStorage'
 
 export const Home = () => {
-    const [newTabname, setNewTabname] = useState('')
+    //const [newTabname, setNewTabname] = useState('')
+    const [newTabname, setNewTabname] = useLocalStorage('newTabname', null)
     const [cache, setCache] = useState([])
     const [data, setData] = useState()
     const [count, setCount] = useState(0)
+    const [isLoading, setIsLoading] = useState(false)
     const stopwatch = useStopwatch();
     
     useEffect(()=> {
@@ -22,11 +25,14 @@ export const Home = () => {
     const load = (tabname) => {
         if (newTabname !=='') {
             stopwatch.start()
+            setIsLoading(true)
             apiGateway.post(`/cache/load/${tabname}`)
             .then(data => { 
                     setData(data.data) 
                     setCount(stopwatch.getElapsedRunningTime())
                     stopwatch.stop()
+                    setIsLoading(false)
+                    getCachedTables()
                 })
             .catch(err => setData(err))
         }
@@ -35,15 +41,30 @@ export const Home = () => {
     }
 
     const unload = (tabname) => {
+        stopwatch.start()
+        setIsLoading(true)
         apiGateway.post(`/cache/unload/${tabname}`)
-            .then(data => setData(data.data))
+            .then(data => { 
+                    setData(data.data)
+                    setCount(stopwatch.getElapsedRunningTime())
+                    stopwatch.stop()
+                    setIsLoading(false)
+                    getCachedTables()
+                })
             .catch(err => setData(err))
     }
-   
+       
+    if (isLoading) 
+        return (
+            <div className='flex justify-center items-center'>
+                <div className='text-4xl py-16'>Please wait while loading...</div>
+            </div>            
+        )
+
     return (
         <div>
             <input type='text' autoFocus placeholder='filename'
-                    className='px-2 py-1' 
+                    className='px-2 py-1' defaultValue={newTabname}
                     onChange={ e=> setNewTabname(e.target.value)}></input>
             <button className='bg-green-700 hover:bg-green-500 text-white text-xs font-bold  uppercase rounded px-2 py-1 mx-2 my-1'
                     onClick={() => { load(newTabname)} }>Load</button>
@@ -58,10 +79,29 @@ export const Home = () => {
                 </li>)
             })}
             </ol>
-            <div className='w-max text-justify' >
-                <p>{ data && JSON.stringify(data) }</p>
-                <p>{ count }</p>                
+
+            <div>
+                <label htmlFor="output" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Output</label>
+                <textarea id="output" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    defaultValue={ data && JSON.stringify(data).concat('\n', 
+                                    `Time elasped is ${count<1000?count:count/1000} ${count<1000?'ms':'s'} `) }
+                    readOnly={true} >
+                </textarea>
             </div>
         </div>
     )
 } 
+
+/*
+   react-use-precision-timer
+   https://www.npmjs.com/package/react-use-precision-timer?activeTab=readme
+
+   useTimer
+   https://justinmahar.github.io/react-use-precision-timer/?path=/story/docs-usetimer--page#timer
+
+   Tailwind CSS Textarea - Flowbite
+   https://flowbite.com/docs/forms/textarea/
+
+   HTML <textarea> 表單多行文字輸入欄位
+   https://www.fooish.com/html/textarea-tag.html
+*/
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,38 +1,55 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { apiGateway } from '../config/apiGateway'
 import { useStopwatch } from "react-use-precision-timer"
+import { useLocalStorage } from '../hook/useLocalStorage'
 
 export const Test = () => {
-    const [tabname, setTabname] = useState('')
+    //const [tabname, setTabname] = useState('')
+    const [tabname, setTabname] = useLocalStorage('tabname', null)
+    const [data, setData] = useState()
     const [count, setCount] = useState(0)
+    const [isLoading, setIsLoading] = useState(false)
     const stopwatch = useStopwatch();
 
-    useEffect (()=> {
-        //inputRef.current.focus();
-    }, [])
     const getFile = () => {
-        if (tabname !== '') {
+        if (tabname !== '') {            
             stopwatch.start()
+            setIsLoading(true)
             apiGateway.get(`/yr/${tabname}`)
                 .then(data => { 
+                                setData(data.data) 
                                 setCount(stopwatch.getElapsedRunningTime())
                                 stopwatch.stop()
+                                setIsLoading(false)
                               }) 
-                .catch(err => console.log(err))
+                .catch(err => setData(err))
         } else {
             alert('Please input filename')
         }
     }
+
+    if (isLoading) 
+        return (
+            <div className='flex justify-center items-center'>
+                <div className='text-4xl py-16'>Please wait while loading...</div>
+            </div>            
+        )
+
     return (
         <div>
             <input type='text' autoFocus placeholder='filename'
-                    className='px-2 py-1'
+                    className='px-2 py-1' defaultValue={tabname}
                     onChange={ e => setTabname(e.target.value)}></input>
             <button className='bg-green-700  hover:bg-green-500 text-white text-xs font-bold  uppercase rounded px-2 py-1 mx-2 my-1'
                     onClick={getFile}>Get</button>
             
-            <div className='w-max text-justify' >
-                <p>{ count }</p>
+            <div>
+                <label htmlFor="output" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Output</label>
+                <textarea id="output" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    defaultValue={ data && JSON.stringify(data).concat('\n', 
+                                    `Time elasped is ${count<1000?count:count/1000} ${count<1000?'ms':'s'} `) }
+                    readOnly={true} >
+                </textarea>
             </div>
         </div>
     )
@@ -44,4 +61,10 @@ export const Test = () => {
 
    useTimer
    https://justinmahar.github.io/react-use-precision-timer/?path=/story/docs-usetimer--page#timer
+
+   Tailwind CSS Textarea - Flowbite
+   https://flowbite.com/docs/forms/textarea/
+
+   HTML <textarea> 表單多行文字輸入欄位
+   https://www.fooish.com/html/textarea-tag.html
 */
